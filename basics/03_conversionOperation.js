d7d14873f930732bb351ea4bea551ad2fd06b80f
afzalakhtar <42388730+afzalakhtar@users.noreply.github.com>
2024-01-23 17:55:33 +0000

    Conversion of datatype

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let numberValue = Number(score)
+// console.log(numberValue)
+// console.log(typeof numberValue)
+
+let isLoggedIn = true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+/*
+1 => true
+0 => false
+"" => fasle
+"Afzal" => true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
